7d41b690b7da1c5dc156d83b1df275b6a03ad74b
radensaleh <m.salehidris13@gmail.com>
2019-05-21 20:48:54 +0700

    fix kuantitas barang

--- resources/views/teknisi/pdfPeminjaman.blade.php
+++ resources/views/teknisi/pdfPeminjaman.blade.php
@@ -38,7 +38,7 @@
           @if($Data->id_pinjam == $data->id_pinjam)
           @foreach($barang as $key => $data_barang)
           @if($data_barang->id_barang == $Data->id_barang )
-          {{ ++$key }}. {{$data_barang->nama_kategori}} {{ $data_barang->nama_merk }} {{ $data_barang->nama_tipe }} <br> 
+          {{ ++$key }}. {{$data_barang->nama_kategori}} {{ $data_barang->nama_merk }} {{ $data_barang->nama_tipe }} ( Kuantitas {{ $Data->kuantitas }} )<br> 
           @endif
           @endforeach
           @endif
